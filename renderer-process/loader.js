f383bde8dbb574412c564656d7c952e3fa0214e2
XIONG Chui <xiongchui.coder@gmail.com>
2017-12-08 14:33:55 +0800

    修复 相机 bug

--- renderer-process/loader.js
+++ renderer-process/loader.js
@@ -1,48 +1,149 @@
-const scene = () => {
-    const options = {
-        canvas: _e('#id-canvas-show'),
+class Renderer {
+    constructor(options) {
+        this.scene = new THREE.Scene()
+        this.renderer = new THREE.WebGLRenderer(options)
+        this.renderer.setClearColor(0xffffff, 1);
+        const {width, height} = this.renderer.domElement
+        this.camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
+        this.camera.position.set(0, 0, 1000)
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement)
+        this.lights = {}
+        this.meshes = {}
+        this.setControls()
+        this.initLights()
+        this.initCube()
+        this.loop()
     }
-    const renderer = new THREE.WebGLRenderer(options)
-    const {width, height} = renderer.domElement
-    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000)
-    camera.position.set(0, 0, 50)
 
-    const s = new THREE.Scene()
-    s.add(new THREE.AmbientLight(0xffffff))
+    setControls() {
+        let controls = this.controls
+        // 使动画循环使用时阻尼或自转 意思是否有惯性
+        controls.enableDamping = true
+        //动态阻尼系数 就是鼠标拖拽旋转灵敏度
+        //controls.dampingFactor = 0.25
+        //是否可以缩放
+        controls.enableZoom = true
+        //是否自动旋转
+        // controls.autoRotate = true
+        //设置相机距离原点的最近距离
+        controls.minDistance = 5
+        //设置相机距离原点的最远距离
+        controls.maxDistance = 7000
+        //是否开启右键拖拽
+        controls.enablePan = true
 
-    const light1 = new THREE.DirectionalLight(0xffffff, 0.75)
-    light1.position.set(0, 100, 100)
-    s.add(light1)
+    }
 
-    const light2 = new THREE.DirectionalLight(0xffffff, 0.75)
-    light2.position.set(0, -100, -100)
-    s.add(light2)
-
-    const controls = new THREE.OrbitControls(camera, renderer.domElement)
-
-    // 使动画循环使用时阻尼或自转 意思是否有惯性
-    controls.enableDamping = true
-    //动态阻尼系数 就是鼠标拖拽旋转灵敏度
-    //controls.dampingFactor = 0.25
-    //是否可以缩放
-    controls.enableZoom = true
-    //是否自动旋转
-    // controls.autoRotate = true
-    //设置相机距离原点的最近距离
-    controls.minDistance = 5
-    //设置相机距离原点的最远距离
-    controls.maxDistance = 600
-    //是否开启右键拖拽
-    controls.enablePan = true
-
-    const loop = () => {
-        requestAnimationFrame(loop)
+    loop() {
+        const controls = this.controls
+        const renderer = this.renderer
+        requestAnimationFrame(() => {
+            this.loop()
+        })
         controls.update()
         renderer.clear()
-        renderer.render(s, camera)
+        renderer.render(this.scene, this.camera)
     }
-    loop()
-    return s
+
+    static single(options) {
+        if (this._instance === undefined) {
+            this._instance = new this(options)
+        }
+        return this._instance
+    }
+
+    addMesh(name, mesh) {
+        const ms = this.meshes
+        ms[name] = mesh
+        this.scene.add(mesh)
+    }
+
+    initLights() {
+        const ambientLight = new THREE.AmbientLight(0x202020);
+        this.addLight('ambient', ambientLight)
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.75);
+        directionalLight.position.x = 1;
+        directionalLight.position.y = 1;
+        directionalLight.position.z = 2;
+        directionalLight.position.normalize();
+        this.addLight('directional', directionalLight);
+
+        const pointLight = new THREE.PointLight(0xffffff, 0.3);
+        pointLight.position.x = 0
+        pointLight.position.y = -25
+        pointLight.position.z = 10
+        this.addLight('point', pointLight)
+    }
+
+    addLight(name, light) {
+        const lights = this.lights
+        const scene = this.scene
+        lights[name] = light
+        scene.add(light)
+    }
+
+    initCube() {
+        // RGB color cube
+        var size = 80;
+        var point;
+        var cubeMaterial = new THREE.MeshBasicMaterial(
+            {color: 0xffffff, vertexColors: THREE.VertexColors});
+        var faceIndices = ['a', 'b', 'c', 'd'];
+        var cubeGeometry = new THREE.CubeGeometry(size, size, size, 1, 1, 1);
+        for (var i = 0; i < cubeGeometry.faces.length; i++) {
+            let face = cubeGeometry.faces[i];
+            // determine if current face is a tri or a quad
+            let numberOfSides = (face instanceof THREE.Face3) ? 3 : 4;
+            // assign color to each vertex of current face
+            for (var j = 0; j < numberOfSides; j++) {
+                let vertexIndex = face[faceIndices[j]];
+                // store coordinates of vertex
+                point = cubeGeometry.vertices[vertexIndex];
+                // initialize color variable
+                let color = new THREE.Color(0xffffff);
+                color.setRGB(0.5 + point.x / size, 0.5 + point.y / size, 0.5 + point.z / size);
+                face.vertexColors[j] = color;
+            }
+        }
+        const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        cube.position.set(100, 50, 0);
+        this.addMesh('cube', cube)
+    }
+
+    setLight(name, options) {
+        const m = this.lights[name]
+        const attrs = ['x', 'y', 'z']
+        attrs.forEach(key => {
+            m.position[key] = options[key]
+        })
+    }
+
+    setLightByGeometry(name, geometry) {
+        const m = this.lights[name]
+        const max = geometry.boundingBox.max
+        const options = {
+            x: max.x * 2,
+            y: max.y * 2,
+            z: max.z * 2,
+        }
+        this.setLight(name, options)
+    }
+
+    setCameraByGeometry(geometry) {
+        const m = this.camera
+        const max = geometry.boundingBox.max
+        const [x, y, z] = [max.x * 3, max.y * 3, max.z * 3]
+        this.setCameraPosition(x, y, z)
+    }
+
+    setCameraPosition(x, y, z) {
+        const m = this.camera
+        m.position.x = x
+        m.position.y = y
+        m.position.z = z
+        this.controls.reset()
+    }
 }
 
 const bindActionDropFile = () => {
@@ -55,18 +156,36 @@ const bindActionDropFile = () => {
         const api = Api.single()
         const p = api.readAsArrayBuffer(file)
         p.then((e) => {
-            const s = scene()
+            const options = {
+                canvas: _e('#id-canvas-show'),
+            }
+            const r = Renderer.single(options)
+            log(r)
             const mat = new THREE.MeshLambertMaterial({
                 color: 0x808080,
                 overdraw: 1,
                 wireframe: false,
                 flatShading: THREE.FlatShading,
-                vertexColors: THREE.FaceColors,
+                vertexColors: THREE.VertexColors,
             })
             const buffer = e.target.result
-            const geom = parsedFileStl(buffer)
-            const obj = new THREE.Mesh(geom, mat)
-            s.add(obj)
+            const vf_data = parsedFileStl(buffer)
+
+            var geo = new THREE.Geometry();
+            geo.vertices = vf_data.vertices;
+            geo.faces = vf_data.faces;
+            geo.computeBoundingBox()
+            geo.computeFaceNormals();
+            geo.computeVertexNormals();
+            geo.center()
+            r.setCameraByGeometry(geo)
+            r.setLightByGeometry('point', geo)
+            r.setLightByGeometry('directional', geo)
+            const mesh = new THREE.Mesh(geo, mat)
+            // setColorRandom(geo)
+            log('geo', geo)
+            log('r', r)
+            r.addMesh('mesh', mesh)
         })
     })
 
--- renderer-process/parser.js
+++ renderer-process/parser.js
@@ -1,28 +1,106 @@
 const binaryVector3 = function (dataView, offset) {
     const dv = dataView
-    const v = new THREE.Vector3()
-    v.x = dv.getFloat32(offset + 0, true)
-    v.y = dv.getFloat32(offset + 4, true)
-    v.z = dv.getFloat32(offset + 8, true)
-    return v
+    let x = dv.getFloat32(offset + 0, true)
+    let y = dv.getFloat32(offset + 4, true)
+    let z = dv.getFloat32(offset + 8, true)
+    return [x, y, z]
 }
 
 const parsedFileStl = (buffer) => {
     // binary STL
-    const view = new DataView(buffer);
-    const size = view.getUint32(80, true);
-    const g = new THREE.Geometry()
-    let offset = 84
-    for (var i = 0; i < size; i++) {
-        const normal = binaryVector3(view, offset)
-        g.vertices.push(binaryVector3(view, offset + 12))
-        g.vertices.push(binaryVector3(view, offset + 24))
-        g.vertices.push(binaryVector3(view, offset + 36))
-        g.faces.push(
-            new THREE.Face3(i * 3, i * 3 + 1, i * 3 + 2, normal))
-        offset += 4 * 3 * 4 + 2
+    const view = new DataView(buffer)
+    let tcount = view.getUint32(80, true)
+    const vert_hash = {}
+    // log('size', size)
+    let pos = 84
+    // for (let i = 0; i < size; i++) {
+    //     const normal = binaryVector3(view, offset)
+    //     g.vertices.push(binaryVector3(view, offset + 12))
+    //     g.vertices.push(binaryVector3(view, offset + 24))
+    //     g.vertices.push(binaryVector3(view, offset + 36))
+    //     g.faces.push(
+    //         new THREE.Face3(i * 3, i * 3 + 1, i * 3 + 2, normal))
+    //     offset += 4 * 3 * 4 + 2
+    // }
+    // log(g)
+    // return g
+    let vertices = []
+    let faces = []
+    while (tcount--) {
+        pos += 12
+        let f1 = view.getFloat32(pos, true);
+        let f2 = view.getFloat32(pos + 4, true);
+        let f3 = view.getFloat32(pos + 8, true);
+        let vertexIndex = vert_hash[[f1, f2, f3]];
+        if (vertexIndex === undefined) {
+            vertexIndex = vertices.length;
+            vertices.push(new THREE.Vector3(f1, f2, f3));
+            vert_hash[[f1, f2, f3]] = vertexIndex;
+        }
+        let v1 = vertexIndex;
+
+        pos += 12;
+
+        f1 = view.getFloat32(pos, true);
+        f2 = view.getFloat32(pos + 4, true);
+        f3 = view.getFloat32(pos + 8, true);
+        vertexIndex = vert_hash[[f1, f2, f3]];
+        if (vertexIndex === undefined) {
+            vertexIndex = vertices.length;
+            vertices.push(new THREE.Vector3(f1, f2, f3));
+            vert_hash[[f1, f2, f3]] = vertexIndex;
+        }
+        let v2 = vertexIndex;
+
+        pos += 12;
+
+        f1 = view.getFloat32(pos, true);
+        f2 = view.getFloat32(pos + 4, true);
+        f3 = view.getFloat32(pos + 8, true);
+        vertexIndex = vert_hash[[f1, f2, f3]];
+        if (vertexIndex === undefined) {
+            vertexIndex = vertices.length;
+            vertices.push(new THREE.Vector3(f1, f2, f3));
+            vert_hash[[f1, f2, f3]] = vertexIndex;
+        }
+        let v3 = vertexIndex;
+        faces.push(new THREE.Face3(v1, v2, v3));
+        pos += 14;
     }
-    return g
+    log('faces', faces)
+    log('ver', vertices)
+    return {vertices:vertices, faces:faces,}
+}
+
+const setColorRandom = (geometry) => {
+    // for (let i = 0; i < geometry.faces.length; i++) {
+    //     geometry.faces[i].color.setHex(Math.random() * 0xffffff)
+    // }
+
+    // for (let i = 0; i < geometry.faces.length; i++) {
+    //     let face = geometry.faces[i];
+    //     for (let j = 0; j < 3; j++) {
+    //         let color = new THREE.Color(Math.random() * 0xffffff)
+    //         face.vertexColors[j] = color
+    //     }
+    // }
+    var size = 10000;
+    var faceIndices = ['a', 'b', 'c', 'd'];
+    for (var i = 0; i < geometry.faces.length; i++) {
+        let face = geometry.faces[i];
+        // determine if current face is a tri or a quad
+        let numberOfSides = (face instanceof THREE.Face3) ? 3 : 4;
+        // assign color to each vertex of current face
+        for (var j = 0; j < numberOfSides; j++) {
+            let vertexIndex = face[faceIndices[j]];
+            // store coordinates of vertex
+            let point = geometry.vertices[vertexIndex];
+            // initialize color variable
+            let color = new THREE.Color(0xffffff);
+            color.setRGB(0.5 + point.x / size, 0.5 + point.y / size, 0.5 + point.z / size)
+            face.vertexColors[j] = color;
+        }
+    }
 }
 
 const parsedFile = (filename, buffer) => {
